4dc16b7a034632c1a14f8f8c02f4b78b5f2e1e5b
WoWPerro Hinojosa <carlo.relion@saei.mx>
2021-03-18 18:51:00 -0600

    Started Menu

---
+++ Assets/Scripts/UI/TextColorChanger.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TextColorChanger : MonoBehaviour
+{
+    private TextMeshProUGUI tmpObj;
+    // Start is called before the first frame update
+    void Start()
+    {
+        tmpObj = GetComponent<TextMeshProUGUI>();
+    }
+
+    // Update is called once per frame
+    public void ChangeColorToWhite()
+    {
+        tmpObj.color = Color.white;
+    }
+
+    public void ChangeColorToBlack()
+    {
+        tmpObj.color = Color.black;
+    }
+}
